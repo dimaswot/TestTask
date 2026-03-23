1e1f994f9b53540d1f878927263c93e788dc4a5f
Дмитрий Григорьев <dimka.grigoriev2@gmail.com>
2026-03-23 15:21:20 +0300

    Добавьте файлы проекта.

---
+++ Services/UserService/User.Application/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+
+namespace User.Application
+{
+    public class Class1
+    {
+    }
+}
